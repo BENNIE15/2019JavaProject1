63a65a6e447aa778269ea25941da4ea6b2f9ee11
Benitp <b.kriel34@gmail.com>
2019-02-13 16:18:00 +0200

    second commit, testing purposes

--- src/main/java/ac/za/cput/App.java
+++ src/main/java/ac/za/cput/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-
+        System.out.println("This a change to see what is reflecting on git status");
 
     }
 }
